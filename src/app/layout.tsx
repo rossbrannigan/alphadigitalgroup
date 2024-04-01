288eff670d7790bf13317c880e7a1fc8d5b1299b
Ross Brannigan 2018 <rossbrannigan2018@Rosss-MacBook-Pro-2.local>
2024-04-01 20:57:03 +0700

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Global management consulting | Alpha Digital Group",
+  description: "Alpha Digital Group is a leading global management consulting firm specializing in providing innovative solutions to businesses across various industries. Our expertise includes strategic planning, digital transformation, and organizational development. Partner with us to drive growth and success for your business.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
