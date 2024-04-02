8741d2bdd5597b6e8304644cd5513568c1303995
Ross Brannigan 2018 <rossbrannigan2018@Rosss-MacBook-Pro-2.local>
2024-04-02 19:34:51 +0700

    Contact Update 14

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,36 +1,27 @@
-import React from "react";
-const ContactPage = () => {
-const ContactForm = (
-<form
-  method="POST"
-  name="contact-form"
-  action="contact/?success=true"
-  data-netlify="true"
->
-<input type="hidden" name="form-name" value="contact-form" />
-    <label htmlFor="name">Name *</label>
-    <input
-      id="name"
-      name="name"
-      required
-      type="text"
-    />
-    <label htmlFor="company">Company *</label>
-    <input id="company" name="company" required type="text" />
-    <label htmlFor="email">E-mail Address *</label>
-    <input id="email" type="email" name="email" required />
-    <label htmlFor="message">Message *</label>
-    <textarea id="message" name="message" required></textarea>
-    <button type="submit">Submit</button>
-  </form>
-);
- 
- return (
-   <div>
-     <h1>Contact Us</h1>
-     {ContactForm}
-   </div>
- );
-};
- 
-export default ContactPage;
+import React from 'react';
+
+export default function ContactPage() {
+  return (
+    <form
+      method="POST"
+      name="contact-form"
+      action="/success"
+      data-netlify="true"
+    >
+      <input type="hidden" name="form-name" value="contact-form" />
+      <div>
+        <label htmlFor="name">Name *</label>
+        <input id="name" name="name" type="text" required />
+      </div>
+      <div>
+        <label htmlFor="email">Email *</label>
+        <input id="email" name="email" type="email" required />
+      </div>
+      <div>
+        <label htmlFor="message">Message *</label>
+        <textarea id="message" name="message" required />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
