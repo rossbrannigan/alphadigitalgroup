96fc5c7b54349bb2cb176487597f6a9eea7c33a3
Ross Brannigan 2018 <rossbrannigan2018@Rosss-MacBook-Pro-2.local>
2024-04-03 16:13:57 +0700

    Added GA 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Head from 'next/head';
 import { GoogleTagManager } from '@next/third-parties/google';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Home() {
   return (
     <div className="bg-gray-100 min-h-screen">
       <Head>
+      <GoogleAnalytics gaId="G-25Y0G3QQE6" />
 
         <title>Global management consulting | Alpha Digital Group</title>
         <link rel="icon" href="/favicon.ico" />
