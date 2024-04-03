2f6f89651db39dfafcbc77f9bc9ecad6beee0099
Ross Brannigan 2018 <rossbrannigan2018@Rosss-MacBook-Pro-2.local>
2024-04-03 13:57:12 +0700

    Added GTM V1.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from '@next/third-parties/google'
+
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+    <main>{children}</main>
+      <GoogleTagManager gtmId="G-25Y0G3QQE6"/>
+
+      </body>
     </html>
   );
 }
