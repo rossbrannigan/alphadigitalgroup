d53feabf0593e3c6f638946d554d5d2dc508c3bd
Ross Brannigan 2018 <rossbrannigan2018@Rosss-MacBook-Pro-2.local>
2024-04-16 11:16:28 +0700

    Changed Capabilities

--- src/app/capabilities/page.tsx
+++ src/app/capabilities/page.tsx
@@ -1,15 +1,16 @@
 // pages/capabilities/CapabilitiesPage.tsx
 
+
 import Image from "next/image";
 import Head from 'next/head';
 import { GoogleTagManager } from '@next/third-parties/google';
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const CapabilitiesPage: React.FC = () => {
   return (
     <div className="bg-gray-100 min-h-screen">
       <Head>
-                               {/* Add Google Analytics tracking tag */}
+        {/* Add Google Analytics tracking tag */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-25Y0G3QQE6"></script>
         <script
           dangerouslySetInnerHTML={{
@@ -20,13 +21,15 @@ const CapabilitiesPage: React.FC = () => {
               gtag('config', 'G-R17NJV1L4N');
             `,
           }}
-                  <GoogleAnalytics gaId="G-25Y0G3QQE6" />
-      <GoogleTagManager gtmId="G-25Y0G3QQE6" />
+        />
+        <GoogleAnalytics gaId="G-25Y0G3QQE6" />
+        <GoogleTagManager gtmId="G-25Y0G3QQE6" />
         <title>Global management consulting | Alpha Digital Group</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Alpha Digital Group - Global management consulting" />
       </Head>
 
+      
       {/* Header */}
       <header className="bg-yellow-400 shadow-lg" style={{ backgroundColor: '#bdff03' }}>
         <div className="container mx-auto py-4 flex items-center justify-between">
