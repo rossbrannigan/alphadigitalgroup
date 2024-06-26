a17b56369537c5d4d10153a76675b0f34ab9fa85
Ross Brannigan 2018 <rossbrannigan2018@Rosss-MacBook-Pro-2.local>
2024-06-26 20:17:10 +0700

    New Edits

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,8 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
-import youtubeSchema from './youtubeSchema'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
-    youtubeSchema,
+    
   ],
 }
