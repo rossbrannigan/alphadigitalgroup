5758e13cb2fff6df5fe59c875db01896782fe10f
Ross Brannigan 2018 <rossbrannigan2018@Rosss-MacBook-Pro-2.local>
2024-07-06 15:29:25 +0700

    Updated About Page

--- types/squidcloud-backend.d.ts
+++
@@ -1,9 +0,0 @@
-// types/squidcloud-backend.d.ts
-declare module '@squidcloud/backend' {
-  export class SquidProject {
-    // Define the properties and methods based on the actual library
-    constructor();
-    // Example method
-    deploy(): void;
-  }
-}
